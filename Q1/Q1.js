39d30ef061f55838cddeb846837aab01802ff4ee
Ahmedkhan1121 <ahmedkhan301270@gmail.com>
2024-05-01 14:21:47 +0500

    firts commit

---
+++ Q1/Q1.js
@@ -0,0 +1,5 @@
+Object.defineProperty(exports, "__esModule", { value: true });
+"use strict";
+//Question 2: Personal Message: Store a person’s name in a variable and print a message to them, like “Hello Ahmed, would you like to learn some TypeScript today?”
+let myName = 'Ahmed';
+console.log(`"hello ${myName},  would you like to learn some TypeScript today?” `);
