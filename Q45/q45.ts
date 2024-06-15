92e51bc29f7fa8fa00b910dc02f72e5b5dbf52f9
Ahmedkhan1121 <ahmedkhan301270@gmail.com>
2024-06-15 15:08:59 +0500

    update

--- Q45/q45.ts
+++ Q45/q45.ts
@@ -6,6 +6,17 @@
 // Print the Object that’s returned to make sure all the information was
 // stored correctly.
 
-let carsFun=(a:string):void=>{
-    let Obj=
+interface Car{
+    company:string;
+    model:string;
+   [key:string]:any;
 }
+const buildCar = (company:string,model:string, extraFeature:{ [key: string]: any } ) :Car => {
+    let car:Car = {
+        company: company,
+        model: model,
+        ...extraFeature,
+    }
+    return car;
+}
+console.log(buildCar('Bently','Flying Spur',{color:'blue',year:2022}));
